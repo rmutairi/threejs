fa7f5e2e219e85c6330c24924dcfe7494c9ef27e
rmutairi <168989497+rmutairi@users.noreply.github.com>
2024-06-07 04:00:40 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,44 +1,41 @@
-
 function init(THREE) {
-    // Init ThreeJS
-    const container = document.querySelector("#canvas-container");
-    const canvas = document.querySelector('#c');
-    const scene = new THREE.Scene();
+  // Init ThreeJS
+  const container = document.querySelector("#canvas-container");
+  container.style.background = 'cadetblue';  // Ensure the container background is transparent
+  const canvas = document.querySelector('#c');
+  const scene = new THREE.Scene();
+  scene.background = null;  // Ensure the scene background is transparent
 
-    // Create renderer
-    const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true, logarithmicDepthBuffer: true });
-    renderer.stencil = true;
-    if (window.devicePixelRatio < 1.5) {
+  // Create renderer
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true, logarithmicDepthBuffer: true });
+  renderer.stencil = true;
+  if (window.devicePixelRatio < 1.5) {
       renderer.setPixelRatio(window.devicePixelRatio);
-    } else {
-    renderer.setPixelRatio(2);
-    }
-    renderer.shadowMapSoft = true;
-    renderer.powerPreference = "high-performance";
-    renderer.shadowMap.enabled = true
-    renderer.gammaFactor = 2.2;
-    renderer.outputEncoding = THREE.sRGBEncoding;
-    renderer.physicallyCorrectLights = true;
+  } else {
+      renderer.setPixelRatio(2);
+  }
+  renderer.shadowMapSoft = true;
+  renderer.powerPreference = "high-performance";
+  renderer.shadowMap.enabled = true;
+  renderer.gammaFactor = 2.2;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.physicallyCorrectLights = true;
 
+  // Setup camera
+  const fov = 17;
+  const aspect = container.clientWidth / container.clientHeight;  // the canvas default
+  const near = 1;
+  const far = 400;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 10, 180);
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-    // Setup camera
-    const fov = 17;
-    const aspect = container.clientWidth / container.clientHeight;  // the canvas default
-    const near = 1;
-    const far = 400;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set(0, 10, 180);
-    camera.lookAt(new THREE.Vector3(0, 0, 0))
+  const render_canvas = renderer.domElement;
+  renderer.setSize(render_canvas.clientWidth, render_canvas.clientHeight, false);
+  camera.aspect = render_canvas.clientWidth / render_canvas.clientHeight;
+  camera.updateProjectionMatrix();
 
-
-    const render_canvas = renderer.domElement;
-    renderer.setSize(render_canvas.clientWidth, render_canvas.clientHeight, false);
-    camera.aspect = render_canvas.clientWidth / render_canvas.clientHeight;
-    camera.updateProjectionMatrix();
-
-    return { scene, renderer, camera }
+  return { scene, renderer, camera };
 }
 
-export {
-    init
-}
+export { init };
